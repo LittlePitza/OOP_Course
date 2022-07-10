9b45d5cf18947550f6c2301732c1819aade48e7f
Pitza <luishc888@gmail.com>
2022-07-10 02:01:43 -0500

    Agregando clases y objetos

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,14 @@
 class Car:
-    id = int
-    license = str
-    driver = str
-    passenger = int
+
+    def __init__(self, id, license, driver):
+        self.id = id
+        self.license = license
+        self.driver = driver
+        self.passenger = 0
+
+    def print_data(self):
+        print('Car ID: ' + str(self.id))
+        print('License: ' + self.license)
+        print('Driver: ' + self.driver)
+        print('Passenger: ' + str(self.passenger))
+        print('-' * 20)
--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,15 @@
 from car import Car
 
 if __name__ == '__main__':
-    car = Car()
-    car.license = 'ABC-123'
-    car.driver = 'John'
-    print('La licencia es :' + car.license)
-    print('El conductor es :' + car.driver)
+    car = Car('ABC-123', 'ABC-123', 'John')
+    car.passenger = 4
+    car.print_data()
+
+    car2 = Car('ABC-456', 'ABC-456', 'Mary')
+    car2.passenger = 8
+    car2.print_data()
+
+    car3 = Car('ABC-789', 'ABC-789', 'Bob')
+    car3.passenger = 2
+    car3.print_data()
+
