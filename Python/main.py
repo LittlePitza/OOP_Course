062ef375f505df13ddeabf9a4f4367763ee71d38
Pitza <luishc888@gmail.com>
2022-07-10 01:50:05 -0500

    Agragando objetos en la clase main de python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,8 @@
+from car import Car
+
 if __name__ == '__main__':
-    print('Hello World!')
+    car = Car()
+    car.license = 'ABC-123'
+    car.driver = 'John'
+    print('La licencia es :' + car.license)
+    print('El conductor es :' + car.driver)
