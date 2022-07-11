44280162e1c71cd7eb3dcabc68ce24045a4537d2
Pitza <luishc888@gmail.com>
2022-07-11 00:31:12 -0500

    Creando constructor

--- Java/Account.java
+++ Java/Account.java
@@ -8,13 +8,6 @@ public class Account {
     String email;
     String password;
 
-//    public Account(Integer id, String name, String document, String email, String password) {
-//        this.id = id;
-//        this.name = name;
-//        this.document = document;
-//        this.email = email;
-//        this.password = password;
-//    }
     public Account(String name, String document) {
         this.name = name;
         this.document = document;
