121b59f0a546dd3879c720eaa06fe9b2085ad599
Pitza <luishc888@gmail.com>
2022-07-11 10:59:03 -0500

    Nuevas clases creadas

--- Javascript/Index.js
+++ Javascript/Index.js
@@ -1,3 +1,8 @@
 let car = new Car("ABC-123", new Account("Juan", "Perez"));
 car.passenger = 4;
 car.printDataCar();
+
+let uberX = new UberX("UberX-456", new Account("Juan", "Perez"),
+    "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
