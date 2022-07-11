407a6210f3e1c8fc87a734597ac49b67dc53d428
Pitza <luishc888@gmail.com>
2022-07-11 10:42:19 -0500

    Creado nuevos tipos de objetos de autos

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,7 @@
 from car import Car
 from account import Account
 from UberX import UberX
+from UberVan import UberVan
 
 if __name__ == '__main__':
 
@@ -18,5 +19,9 @@ if __name__ == '__main__':
     # Create an instance of UberX
     uberX = UberX('Pitza-123', Account('John', '123456789'), 'BMW', '2017')
     uberX.print_data()
+
+    # Create an instance of UberVan
+    uberVan = UberVan('Pitza-123', Account('John', '123456789'), ['A', 'B'], ['Leather', 'Plastic'])
+    uberVan.print_data()
 
 
